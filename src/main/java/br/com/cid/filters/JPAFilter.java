07dfbb3817dcc878349307dda3fccac4691d54d0
gilsonsilvati <gilsonsilvati@gmail.com>
2015-09-29 07:37:15 -0300

    Teste da primeira versão

--- src/main/java/br/com/cid/filters/JPAFilter.java
+++ src/main/java/br/com/cid/filters/JPAFilter.java
@@ -1,6 +1,18 @@
 package br.com.cid.filters;
 
-/*
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
 @WebFilter(servletNames = {"Faces Servlet"})
 public class JPAFilter implements Filter {
 	
@@ -52,4 +64,3 @@ public class JPAFilter implements Filter {
 	}
 
 }
-*/
